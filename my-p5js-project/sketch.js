5c54a52b7c10c131ed924428732950368d32bdad
Bernardo Castillo <bcasti079@west-mec.org>
2026-05-14 13:00:30 -0700

    added button to save the canvas as untitled.png

--- my-p5js-project/sketch.js
+++ my-p5js-project/sketch.js
@@ -1,39 +1,39 @@
 let borderwidth = document.getElementById("borderwidth");
 
+// Event Listeners to make sure border width is greater than 1 but less than 100 at all times
+borderwidth.addEventListener("keyup", checkNum);
+borderwidth.addEventListener("click", checkNum);
 function setup() {
   // Create the canvas
   createCanvas(1200, 700);
+  // Create color picker
   myPicker = createColorPicker("black");
   myPicker.position(80, 50);
-
-  // Title for color
+  // Title for color picker
   let p = createP("Color");
   p.position(85, 30);
   // Set background to black
   background(255);
-
-  // Set width of the lines
-
+  // Function in undo.js
   saveState();
-
   // Set screen reader accessible description
   describe("A blank canvas where the user draws by dragging the mouse");
+
+  // Button in html that save canvas without resetting
+  document.getElementById('canvasSave').addEventListener('click', () => {
+    saveCanvas();
+  })
 }
 
-// Event Listeners to make sure border width is greater than 1 but less than 100 at all times
-borderwidth.addEventListener("keyup", checkNum);
-borderwidth.addEventListener("click", checkNum);
-
+// Function for regex
 function checkNum() {
   console.log("test");
   if (borderwidth.value >= 100) borderwidth.value = 100;
-  console.log(borderwidth.value); // Force cap at 100
-  if (borderwidth.value <= 1) borderwidth.value = 1; // Force floor at 0
+  console.log(borderwidth.value); 
+  if (borderwidth.value <= 1) borderwidth.value = 1; 
 };
-
+ 
 function mouseDragged() {
-  // Set the color based on the mouse position, and draw a line
-  // from the previous position to the current position
   strokeWeight(borderwidth.value);
   stroke(myPicker.color(), 90, 90);
   line(pmouseX, pmouseY, mouseX, mouseY);
